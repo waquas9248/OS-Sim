7c8e0db611e6fb5b588b004d39050f9dc4431767
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2024-06-18 18:00:43 -0500

    Create Pipes.c

---
+++ Pipes.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+int pid,a[20];
+char str[30],buf[30];
+pipe(a);
+pid=fork();
+if(pid > 0)
+{
+strcpy(str,"welcome to ipc using pipes");
+write(a[1],str,sizeof(str));
+}
+else if(pid==0)
+{
+read(a[0],buf,sizeof(buf));
+printf("msg received by child is %s\n",buf);
+}
+else
+printf("error in creation");
+}
