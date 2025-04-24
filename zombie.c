429757fc91d12f476e7f37977bab55894607eeb0
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2025-04-24 10:16:26 -0500

    Update zombie.c

--- zombie.c
+++ zombie.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
+
 main()
 {
-int pid;
-printf("demo of zombie process");
-pid=fork();
-if(pid==0)
-{
-printf("\nchild process pid = %d\n",getpid());
-printf("\nparent process pid(from child) = %d\n",getppid());
-}
-else if(pid>0)
-{
-sleep(5);
-printf("\nparent process pid = %d\n\n",getpid());
-system("ps -l");
-}
-else
-printf("error in creation");
+    int pid;
+    printf("demo of zombie process");
+    pid=fork();
+    if(pid==0){
+        printf("\nchild process pid = %d\n",getpid());
+        printf("\nparent process pid(from child) = %d\n",getppid());
+    }
+    else if(pid>0){
+        sleep(5);
+        printf("\nparent process pid = %d\n\n",getpid());
+        system("ps -l");
+    }
+    else
+        printf("error in creation");
 }
