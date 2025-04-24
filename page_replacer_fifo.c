75b10eea98edf92f8f20803a3dfb6c90cc62d488
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2025-04-24 10:11:47 -0500

    Update page_replacer_fifo.c

--- page_replacer_fifo.c
+++ page_replacer_fifo.c
@@ -1,38 +1,36 @@
 #include<stdio.h>
+
 main()
 {
-int i,n,a[50],frame[10],fs,k,avail,count=0,loc;
-printf("Enter the length of ref string : ");
-scanf("%d",&n);
-printf("Enter reference string : \n");
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
-printf("Enter frame size : ");
-scanf("%d",&fs);
-for(i=0;i<fs;i++)
-frame[i]=-1;
-loc=0;
-printf("\nref_str\tpageframe\n");
-for(i=0;i<n;i++)
-{
-printf("%d\t",a[i]);
-avail=0;
-for(k=0;k<fs;k++)
-{
-if(frame[k]==a[i])
-avail=1;
-}
-if(avail==0)
-{
-frame[loc]=a[i];
-loc=(loc+1)%fs;
-count++;
-for(k=0;k<fs;k++)
-printf("%d\t",frame[k]);
-}
-printf("\n");
-}
-printf("no.of page faults = %d\n",count);
+    int i,n,a[50],frame[10],fs,k,avail,count=0,loc;
+    printf("Enter the length of ref string : ");
+    scanf("%d",&n);
+    printf("Enter reference string : \n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    printf("Enter frame size : ");
+    scanf("%d",&fs);
+    for(i=0;i<fs;i++)
+        frame[i]=-1;
+    loc=0;
+    printf("\nref_str\tpageframe\n");
+  
+    for(i=0;i<n;i++){
+        printf("%d\t",a[i]);
+        avail=0;
+        for(k=0;k<fs;k++){
+            if(frame[k]==a[i])
+            avail=1;
+        }
+        if(avail==0){
+          frame[loc]=a[i];
+          loc=(loc+1)%fs;
+          count++;
+          for(k=0;k<fs;k++)
+          printf("%d\t",frame[k]);
+        }
+        printf("\n");
+    }
+
+    printf("no.of page faults = %d\n",count);
 }
