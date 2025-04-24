8bd9a0177c1c2b1f4360488dff07f52ea0bdfde6
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2025-04-24 10:05:27 -0500

    Update file_mngr.c

--- file_mngr.c
+++ file_mngr.c
@@ -1,33 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<fcntl.h>
+
 main(int argc,char *argv[])
 {
-int fd1,fd2,n;
-char buffer[10];
-fd1=open(argv[1],O_RDONLY);
-fd2=open(argv[2],O_WRONLY|O_CREAT,0644);
-if((fd1<0)||(fd2<0))
-{
-printf("FAILED IN OPENING THE FILE\n");
-exit(0);
-}
-while(1)
-{
-n=read(fd1,buffer,1);
-if(n==0)
-break;
-write(fd2,buffer,1);
-}
-close(fd1);
-close(fd2);
-fd2=open(argv[2],O_RDONLY);
-while(1)
-{
-n=read(fd2,buffer,1);
-if(n==0)
-break;
-write(1,buffer,1);
-}
-close(fd2);
+    int fd1,fd2,n;
+    char buffer[10];
+    fd1=open(argv[1],O_RDONLY);
+    fd2=open(argv[2],O_WRONLY|O_CREAT,0644);
+    if((fd1<0)||(fd2<0))
+    {
+        printf("FAILED IN OPENING THE FILE\n");
+        exit(0);
+    }
+    while(1)
+    {
+        n=read(fd1,buffer,1);
+        if(n==0)
+        break;
+        write(fd2,buffer,1);
+    }
+    close(fd1);
+    close(fd2);
+    fd2=open(argv[2],O_RDONLY);
+    while(1)
+    {
+        n=read(fd2,buffer,1);
+        if(n==0)
+        break;
+        write(1,buffer,1);
+    }
+    close(fd2);
 }
