7948b601a18c2f8e90b3bfb44f8074898ffa2d28
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2025-04-24 10:10:32 -0500

    Update orphan.c

--- orphan.c
+++ orphan.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
+
 main()
 {
-int pid;
-printf("\ndemo of orphan process\n");
-pid=fork();
-if(pid==0)
-{
-sleep(10);
-printf("\nchild process pid = %d",getpid());
-printf("\nparent process pid(from child) = %d\n",getppid());
-}
-else if(pid>0)
-printf("\nparent process pid = %d\n",getpid());
-else
-printf("error in creation\n");
+    int pid;
+    printf("\ndemo of orphan process\n");
+    pid=fork();
+    if(pid==0){
+        sleep(10);
+        printf("\nchild process pid = %d",getpid());
+        printf("\nparent process pid(from child) = %d\n",getppid());
+    }
+    else if(pid>0)
+        printf("\nparent process pid = %d\n",getpid());
+    else
+        printf("error in creation\n");
 }
