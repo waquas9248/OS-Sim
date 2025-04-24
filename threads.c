b12128ca40f7765260aae7f515b850c5bb7a0f53
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2025-04-24 10:15:59 -0500

    Update threads.c

--- threads.c
+++ threads.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 #include<pthread.h>
+
 void *runner();
 main()
 {
-int i;
-printf("demo of thread\n");
-pthread_t tid;
-pthread_create(&tid,NULL,runner,0);
-pthread_join(tid,NULL);
-for(i=0;i<5;i++)
-{
-printf("inside main\n");
-sleep(1);
+    int i;
+    printf("demo of thread\n");
+    pthread_t tid;
+    pthread_create(&tid,NULL,runner,0);
+    pthread_join(tid,NULL);
+    for(i=0;i<5;i++){
+        printf("inside main\n");
+        sleep(1);
+    }
 }
-}
-void *runner()
-{
-int i;
-for(i=0;i<5;i++)
-{
-//sleep(2);
-printf("inside thread\n");
-sleep(1);
-}
-pthread_exit(0);
+
+void *runner(){
+    int i;
+    for(i=0;i<5;i++)
+    {
+        //sleep(2);
+        printf("inside thread\n");
+        sleep(1);
+    }
+    pthread_exit(0);
 }
